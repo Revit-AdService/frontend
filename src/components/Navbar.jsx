7c8cd1ca6809a00cdb8ed51cab770df0bb3964f4
Tauya <ayuat2003@gmail.com>
2023-05-22 11:18:30 +0200

    Updated the navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,23 +2,58 @@ import React from "react";
 import logo from "../assets/images/Logo.png";
 import { ArrowDropDown, Menu, ShoppingCart } from "@mui/icons-material";
 import { Avatar, Button } from "@mui/material";
-import '../assets/Navbar.scss'
 
 const Navbar = () => {
   return (
     <div className="navbar cont">
       <img src={logo} alt="Revit AdService" />
-      <Button className="Button" varient="text" endIcon={<ArrowDropDown />}>
+      <Button
+        sx={{
+          color: "#242254",
+          fontSize: "7px",
+        }}
+        varient="text"
+        endIcon={
+          <ArrowDropDown
+            sx={{
+              marginLeft: "-13px",
+              marginTop: "-4px",
+            }}
+          />
+        }
+      >
         Category
       </Button>
-      <Button className="Button " varient="text" endIcon={<ShoppingCart />}>
+      <Button
+        sx={{
+          color: "#242254",
+          fontSize: "7px",
+        }}
+        varient="text"
+        endIcon={
+          <ShoppingCart
+            sx={{
+              marginLeft: "-7px",
+              marginTop: "-3px",
+              width: "10px",
+              height: "10px",
+            }}
+          />
+        }
+      >
         CART
       </Button>
-      <Button className="Button" varient="text">
+      <Button
+        sx={{
+          color: "#242254",
+          fontSize: "7px",
+        }}
+        varient="text"
+      >
         signup/login
       </Button>
-      <Avatar/>
-      <Menu className="btn-menu"/>
+      <Avatar />
+      <Menu sx={{ marginLeft: "5px" }} />
     </div>
   );
 };
